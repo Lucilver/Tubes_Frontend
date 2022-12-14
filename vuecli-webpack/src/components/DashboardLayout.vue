9673fc2ec7575b11a42bfe434f80f3e9129bc046
GrayfienHalim <grayfien2002@gmail.com>
2022-12-14 14:39:45 +0700

    tampilan dashboard dan product

--- vuecli-webpack/src/components/DashboardLayout.vue
+++ vuecli-webpack/src/components/DashboardLayout.vue
@@ -1,38 +1,40 @@
 <template>
   <div class="dashboard">
-    <v-navigation-drawer class="fullheight" width="256" v-model="drawer" absolute temporary color="white">
+      <v-navigation-drawer class="fullheight" width="256" v-model="drawer"
+      absolute temporary color="grey lighten-5">
       <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">Kelompok J</v-list-item-title>
-          <v-list-item-subtitle>WEB C</v-list-item-subtitle>
-        </v-list-item-content>
+          <v-list-item-content>
+              <v-list-item-title class="title">Kelompok J</v-list-item-title>
+              <v-list-item-subtitle>Web C</v-list-item-subtitle>
+          </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link color="light-blue darken-4" tag="router-link" :to="item.to">
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+          <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          color="light-blue darken-4"
+          tag="router-link"
+          :to="item.to"
+          >
+              <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
       </v-list>
-    </v-navigation-drawer>
-    <v-app-bar color="red lighten-3" dark>
-      <v-app-bar-nav-icon @click="drawer = true" color="white"></v-app-bar-nav-icon>
-      <VSpacer />
-    </v-app-bar>
-    <v-app-bar app fixed height="75px" class="grey">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <VSpacer />
-      <v-toolbar-items>
-        <v-btn text router @click="logout"><v-icon>mdi-power</v-icon></v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
-
-    <div class="fullheight pa-5">
-      <router-view></router-view>
-    </div>
+      </v-navigation-drawer>
+      <v-app-bar color="black" dark>
+          <v-app-bar-nav-icon
+          @click="drawer = true"
+          color="white"></v-app-bar-nav-icon>
+          <VSpacer />
+      </v-app-bar>
+      <div class="fullheight pa-5">
+          <router-view></router-view>
+      </div>
   </div>
 </template>
 <script>
